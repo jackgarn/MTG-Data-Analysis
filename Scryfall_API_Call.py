1741629f84e7479563e48fa8b7dd3df8ad16a3fc
Jack Garn <113713405+jackgarn@users.noreply.github.com>
2023-02-02 12:37:28 -0700

    Update and rename Scryfall_API_Call.py to Oracle_Card_cleaning.py
    changed to the final version with one file for each dataset, no prints, each file calls and cleans and outputs data independently :)

--- Scryfall_API_Call.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-# Gets the JSON for ALL MTG Cards
-response = requests.get('https://data.scryfall.io/default-cards/default-cards-20230120100454.json')
-# stores the call in a json file
-with open("Cards.json","w") as file:
-    json.dump(response.json(),file,indent = 4)
